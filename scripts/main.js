37b77a360dc3b9efeea409c78e2715a51f7f422c
Caio J. Carvalho <cjlcarvalho@live.com>
2017-07-30 00:41:07 -0300

    My website

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,26 @@
 // setting my actual age
-const setActualAge = function(){
-    let birth = new Date(1995, 6, 7);
-    let diff = Math.abs((new Date()).getTime() - birth.getTime());
-    let diffdays = Math.floor(Math.ceil(diff / (1000 * 3600 * 24)) / 365);
+const setActualAge = () => {
+    let difftime = Math.abs((new Date()).getTime() - (new Date(1995, 6, 7)).getTime());
+    let diffdays = Math.floor(Math.ceil(difftime / (1000 * 3600 * 24)) / 365);
     let age = document.querySelector('[id*="age"]');
     age.innerHTML = diffdays + age.innerHTML;
 }
 
+// controlling the display of some items
+const displayItems = () => {
+    $('.titulo').toArray().forEach((item) => {
+        $(item).click((event) => {
+            $('.itens').toArray().forEach((elemento) => {
+                if($(elemento).parent()[0] === $(item).parent()[0])
+                    $(elemento).toggle('show');
+                else if($(elemento).is(':visible'))
+                    $(elemento).hide();
+            });
+        });
+    });
+}
+
 $(document).ready(() => {
-    $('.t1').click((event) => {
-        $('.i1').toggle('show');
-        if($('.i2').is(':visible'))
-            $('.i2').hide();
-        if($('.i3').is(':visible'))
-            $('.i3').hide();
-    });
-    $('.t2').click((event) => {
-        $('.i2').toggle('show');
-        if($('.i1').is(':visible'))
-            $('.i1').hide();
-        if($('.i3').is(':visible'))
-            $('.i3').hide();
-    });
-    $('.t3').click((event) => {
-        $('.i3').toggle('show');
-        if($('.i2').is(':visible'))
-            $('.i2').hide();
-        if($('.i1').is(':visible'))
-            $('.i1').hide();
-    });
+    displayItems();
     setActualAge();
 });
